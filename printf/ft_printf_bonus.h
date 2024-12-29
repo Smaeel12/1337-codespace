bf6ac05974e4f69adecf728b415bdbb8489acd42
Smaeel12 <125469789+Smaeel12@users.noreply.github.com>
2024-12-29 09:41:08 +0000

    add files

--- printf/ft_printf_bonus.h
+++ printf/ft_printf_bonus.h
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_bonus.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iboubkri <iboubkri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 10:15:59 by iboubkri          #+#    #+#             */
-/*   Updated: 2024/12/29 09:05:27 by iboubkri         ###   ########.fr       */
+/*   Updated: 2024/12/29 09:07:13 by iboubkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef FT_PRINTF_BONUS_H
+# define FT_PRINTF_BONUS_H
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
+typedef struct s_conver
+{
+	char	flag;
+	char	cv;
+}			t_conver;
+
+typedef struct s_bs
+{
+	char	*bs;
+	int		lb;
+	char	*pre;
+}			t_bs;
+
+typedef struct s_bss
+{
+	t_bs	bss[3];
+}			t_bss;
+
+int			ft_printf(const char *format, ...);
 
 #endif
--- printf/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-
-    printf("%010d\n%010d\n%010d\n", 102, 13, 2131);
-}
