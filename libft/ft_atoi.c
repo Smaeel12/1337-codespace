9de9a7a576e9e1a720c934bc77a2ff1e91960fb9
Smaeel12 <125469789+Smaeel12@users.noreply.github.com>
2024-12-31 11:21:07 +0000

    libft

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: iboubkri <iboubkri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:20:37 by iboubkri          #+#    #+#             */
-/*   Updated: 2024/11/15 13:51:58 by iboubkri         ###   ########.fr       */
+/*   Updated: 2024/12/31 11:20:53 by iboubkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 int	ft_atoi(const char *str)
 {
-	size_t	i;
 	size_t	res;
 	int		sign;
 
-	i = 0;
 	res = 0;
 	sign = 1;
-	while (((str[i] >= 9 && str[i] <= 13) || str[i] == ' '))
-		i++;
-	if ((str[i] == '-' || str[i] == '+'))
-		sign = (44 - str[i++]);
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = (str[i] - '0') + res * 10;
-		i++;
-	}
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+		sign = (44 - *(str++));
+	while (*str >= '0' && *str <= '9')
+		res = (res << 3) + (res << 1) + (*(str++) - '0');
 	return ((int)(res * sign));
 }
